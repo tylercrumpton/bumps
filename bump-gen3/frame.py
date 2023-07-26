3a5330b00ae6e538772a4fb20065d3cd47c4b552
Hunter Fuller <hfuller@pixilic.com>
2023-07-25 22:25:58 -0500

    Implement section support in the GenericBump template.

--- bump-gen3/frame.py
+++ bump-gen3/frame.py
@@ -1,28 +1,33 @@
+import sys, os
 from PIL import Image, ImageDraw, ImageFont
 
 appearance = "GenericBump"
-#message = "Dear DCTV,\nI heard some rumors about the Dragon Con\nmemberships and badges that feel strange and I\nwant you to help me suss out if they're fact or fiction.\nThanks!\n[ Andrew ]\n\nDear Andrew,\nSure, let's have at em."
-message = "Generic Bump - start at top of safe area\n\nSkip a line between paragraphs\n\nMove to new page as necessary and avoid putting\ntext that overlaps the bug in the corner\n\nPage cuts should be in sync with music\nto make transitions 'smooth'"
-W,H = 1920,1080
+W,H = 1920,1080 #HACK - hardcoding ( we should consume it from the file)
 
-#img = Image.open("sample_in.jpg")
+with open(sys.argv[1], 'r') as f:
+    message = f.read()
+
 #img = Image.new('RGB', (W,H), 'black')
 img = Image.open('Generic Template 16x9 - black and bug.png')
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype("HelveticaNeue-CondensedBold-05.ttf", 72)
 
-#multiline shit
-lines = message.splitlines();
-print(len(lines), "lines")
-
-_,_,_,lineheight = draw.textbbox((0,0), lines[0], font=font)
-print("Line height is", lineheight)
-
 if appearance == "GenericBump":
-    #draw.text((W/2, H/2), message, anchor="mm", align="left", font=font, fill='white', spacing=20)
-    draw.text((192,89), message, anchor="la", align="left", font=font, fill='white', spacing=16)
+    sections = message.split(2*os.linesep)
+    print(len(sections), "sections")
+
+    section_message = ""
+    for i in range(len(sections)):
+        print("Section", i)
+        section_message = section_message + sections[i] + "\n\n"
+        print("Drawing text:", section_message)
+        draw.text((192,89), section_message, anchor="la", align="left", font=font, fill='white', spacing=16)
+        
+        filename = sys.argv[1] + " - IMG" + str(i+1).zfill(4) + ".png"
+        print("WRITING:", filename)
+        img.save(filename)
+
 else:
     draw.text((W/2, H/2), message, anchor="mm", align="center", font=font, fill='white')
 
 
-img.save('sample-out.png')
